46cff608a2adf591d17dd20beb007a9628f3180e
Студент <student@ad.anosov.ru>
2023-05-30 14:57:00 +0500

    admin

---
+++ UsmanovBochkarevKhayrullin/ViewModels/AdminWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UsmanovBochkarevKhayrullin.Models;
+
+namespace UsmanovBochkarevKhayrullin.ViewModels
+{
+    public class AdminWindowViewModel : ViewModelBase
+    {
+        private ObservableCollection<Book> _books;
+        public ObservableCollection<Book> Books
+        {
+            get => _books;
+            set => this.RaiseAndSetIfChanged(ref _books, value);
+        }
+
+        private User user { get; set; }
+
+        public AdminWindowViewModel()
+        {
+            LibraryContext libraryContext = new LibraryContext();
+            libraryContext.Users.Load();
+            libraryContext.Books.Load();
+            Books = libraryContext.Books.Local.ToObservableCollection();
+        }
+        public AdminWindowViewModel(User user): this()
+        {
+            this.user = user;
+        }
+    }
+}
---
+++ UsmanovBochkarevKhayrullin/Views/AdminWindow.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UsmanovBochkarevKhayrullin.Views;
+
+public partial class AdminWindow : Window
+{
+    public AdminWindow()
+    {
+        InitializeComponent();
+    }
+
+}
