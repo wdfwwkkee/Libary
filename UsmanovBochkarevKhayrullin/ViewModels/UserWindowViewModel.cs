2ad5164033aa9f9d1c4b22cddce52149bef25c2d
Студент <student@ad.anosov.ru>
2023-05-31 15:04:19 +0500

    UserWindow

--- UsmanovBochkarevKhayrullin/ViewModels/UserWindowViewModel.cs
+++ UsmanovBochkarevKhayrullin/ViewModels/UserWindowViewModel.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsmanovBochkarevKhayrullin.Models;
 
 namespace UsmanovBochkarevKhayrullin.ViewModels
 {
     public class UserWindowViewModel : ViewModelBase
     {
+        private ObservableCollection<Book> _books;
+        public ObservableCollection<Book> Books
+        {
+            get => _books;
+            set => this.RaiseAndSetIfChanged(ref _books, value);
+        }
+
+        public User user { get; set; }
+
+        public UserWindowViewModel()
+        {
+            LibraryContext libraryContext = new LibraryContext();
+            libraryContext.Users.Load();
+            libraryContext.Books.Load();
+            Books = libraryContext.Books.Local.ToObservableCollection();
+        }
+        public UserWindowViewModel(User user) : this()
+        {
+            this.user = user;
+        }
     }
 }
